3ade7d26d3ea1d29835e5c1c49c04a2b4f95e24c
lucapette <lucapette@gmail.com>
2011-04-29 17:28:39 +0200

    code_dirs aliases only for dirs

--- bash/code_dirs.rb
+++ bash/code_dirs.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/ruby 
 
 Dir["#{File.expand_path("~/code")}/*"].each { |file|
-    cmd=File.basename(file)
-    puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
+    if File.directory?(file)
+        cmd=File.basename(file)
+        puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
+    end
 }
