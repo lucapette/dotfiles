a54ce76584bdc11d85e461cd3e3bffcfdf0649ce
lucapette <lucapette@gmail.com>
2011-12-03 18:08:16 +0100

    fix code_dirs

--- bash/code_dirs.rb
+++ bash/code_dirs.rb
@@ -1,8 +1,8 @@
-#!/usr/bin/ruby 
+#!/usr/bin/env ruby
 
-Dir["#{File.expand_path("~/code")}/*"].each { |file|
-    if File.directory?(file)
-        cmd=File.basename(file)
-        puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
-    end
-}
+Dir["#{File.expand_path("~/code")}/*"].each do |file|
+  if File.directory?(file)
+    cmd = File.basename(file)
+    puts "alias #{cmd}=\"cd #{file}\"" if `which #{cmd}`.length == 0
+  end
+end
