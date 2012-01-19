2c1d2489114ce61cced70893b4be60f24f8e99a3
lucapette <lucapette@gmail.com>
2012-01-19 23:32:45 +0100

    fix whitespace when not in a git repo

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[green]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[green]%} "
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}✗%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[blue]%} ?%{$reset_color%}"
@@ -8,6 +8,6 @@ function rvm_prompt() {
   echo "%{$fg_bold[green]%}$(~/.rvm/bin/rvm-prompt v g)"
 }
 
-PROMPT="\$(rvm_prompt) %{$fg_bold[cyan]%}%c %{$reset_color%}"
+PROMPT="\$(rvm_prompt) %{$fg_bold[cyan]%}%c%{$reset_color%}"
 PROMPT="%{$PROMPT%}\$(git_prompt_info)\$(git_prompt_status)"
 PROMPT="%{$PROMPT%}%{$fg_bold[red]%} ➜%{$reset_color%} "
