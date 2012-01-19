718935adf5409cc343cd49657ab2cde36fb8d952
lucapette <lucapette@gmail.com>
2012-01-19 23:29:47 +0100

    add untracked git files to PROMPT

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,13 @@
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}✗%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[blue]%} ?%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
 
 function rvm_prompt() {
-echo "%{$fg_bold[green]%}$(~/.rvm/bin/rvm-prompt v g)"
+  echo "%{$fg_bold[green]%}$(~/.rvm/bin/rvm-prompt v g)"
 }
 
-PROMPT="\$(rvm_prompt) %{$fg_bold[cyan]%}%c %{$reset_color%}\$(git_prompt_info)%{$fg_bold[red]%} ➜%{$reset_color%} "
+PROMPT="\$(rvm_prompt) %{$fg_bold[cyan]%}%c %{$reset_color%}"
+PROMPT="%{$PROMPT%}\$(git_prompt_info)\$(git_prompt_status)"
+PROMPT="%{$PROMPT%}%{$fg_bold[red]%} ➜%{$reset_color%} "
