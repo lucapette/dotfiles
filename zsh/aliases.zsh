c9a529d5562f57d7a909dcd1409ae5bacdee60fb
lucapette <lucapette@gmail.com>
2012-10-01 20:26:23 +0200

    Add mac aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,16 +1,14 @@
-alias cr2jpg='ufraw-batch *.CR2 --wb=auto --exposure=1.0 --out-type=jpeg --compression=100'
 alias git='hub'
 alias g='git'
 alias h='history'
 alias jekyll='jekyll --server --pygments'
-alias ls='ls -hF --color' # add colors for filetype recognition
+alias ls='ls -hFG' # add colors for filetype recognition
 alias mi='vi `find  db/migrate/* | tail -1`'
 alias mkdir='mkdir -p'
-alias nautilus='nautilus .'
+alias o='open .'
 alias ps='ps aux | more'
 alias psgrep='ps | grep'
 alias rm='rm -i'
-alias s='rails s'
 alias tmux='tmux -2'
 alias vi='vim'
 alias vim='vim -O'
@@ -27,6 +25,3 @@ alias 6='cd +6'
 alias 7='cd +7'
 alias 8='cd +8'
 alias 9='cd +9'
-
-# Aliases for extensions
-alias -s rb=vim
