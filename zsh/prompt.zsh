a3f3114bff89cfcb27e78ddc1872c7af084d40ae
lucapette <lucapette@gmail.com>
2013-11-24 18:38:23 +0100

    New prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,32 +1,35 @@
-# Setup the prompt with pretty colors
 setopt prompt_subst
 
 RESET_COLOR="%f%b"
 
-function ruby_version() { echo "%B%F{green}$(chruby | grep '*' | tr -d '* ' | sed 's/ruby-//')" }
-
-function git_branch_name() {
-  ref=$(git symbolic-ref --short HEAD 2> /dev/null)
-  if [ -z "$ref" ]; then
-    ref=$(git reflog 2> /dev/null | grep checkout | head -n1 | awk '{print $NF}')
-  fi
-  if [ -z "$ref" ]; then
-    return
-  fi
-  echo " %B%F{green}$ref$RESET_COLOR"
+__where() {
+        echo "%B%F{yellow}%n%B%F{white}@%F{blue}%m$RESET_COLOR"
 }
 
-# Get the status of the working tree
-function git_status() {
-  STATUS=""
-  if [[ -n $(git status -s 2> /dev/null) ]]; then
-    STATUS="%B%F{yellow}✗$RESET_COLOR"
-  fi
-  INDEX=$(git status --porcelain 2> /dev/null)
-  if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
-    STATUS="$STATUS%B%F{blue} ?$RESET_COLOR"
-  fi
-  echo $STATUS
+__ruby() { echo "%B%F{yellow}$(chruby | grep '*' | tr -d '* ' | sed 's/ruby-//')" }
+
+__branch() {
+        ref=$(git symbolic-ref --short HEAD 2> /dev/null)
+        if [ -z "$ref" ]; then
+                ref=$(git reflog 2> /dev/null | grep checkout | head -n1 | awk '{print $NF}')
+        fi
+        if [ -z "$ref" ]; then
+                return
+        fi
+        echo " %B%F{green}$ref$RESET_COLOR"
 }
 
-PROMPT='$(ruby_version) %B%F{cyan}%c$RESET_COLOR$(git_branch_name)$(git_status)%F{red} ➜ $RESET_COLOR'
+__status() {
+        STATUS=""
+        if [[ -n $(git status -s 2> /dev/null) ]]; then
+                STATUS="%F{yellow}✗$RESET_COLOR"
+        fi
+        INDEX=$(git status --porcelain 2> /dev/null)
+        if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
+                STATUS="$STATUS%F{blue} ?$RESET_COLOR"
+        fi
+        echo $STATUS
+}
+
+PROMPT='$(__ruby) %B%F{cyan}%c$RESET_COLOR$(__branch)$(__status)%F{red} ➜ $RESET_COLOR'
+RPROMPT='$(__where)'
