4788598ad8008603612b2b6ac923a249a929d004
lucapette <lucapette@gmail.com>
2013-12-24 16:32:45 +0100

    Add more commands for slate

--- slate.js
+++ slate.js
@@ -5,6 +5,38 @@ var fullscreen = slate.operation("move", {
     "height" : "screenSizeY"
 });
 
+var halfLeft = slate.operation("move", {
+    "x" : "screenOriginX",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX/2",
+    "height" : "screenSizeY"
+});
+
+var halfRight = slate.operation("move", {
+    "x" : "screenOriginX+screenSizeX/2",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX/2",
+    "height" : "screenSizeY"
+});
+
 slate.bind("m:ctrl;alt;cmd", function(win) {
     win.doOperation(fullscreen);
 });
+
+slate.bind("left:ctrl;alt;cmd", function(win) {
+    win.doOperation(halfLeft);
+});
+
+slate.bind("right:ctrl;alt;cmd", function(win) {
+    win.doOperation(halfRight);
+});
+
+var adium = slate.operation("focus", {"app" : 'Adium'});
+var chrome = slate.operation("focus", {"app" : 'Google Chrome'});
+var skype = slate.operation("focus", {"app" : 'Skype'});
+var iterm = slate.operation("focus", {"app" : 'iTerm'});
+
+slate.bind("j:ctrl;alt;cmd", chrome);
+slate.bind("l:ctrl;alt;cmd", adium);
+slate.bind("k:ctrl;alt;cmd", skype);
+slate.bind("i:ctrl;alt;cmd", iterm);
