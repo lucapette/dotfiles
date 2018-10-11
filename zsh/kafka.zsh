3ae51842acb0967d682e1b21208ed22204b5f3ca
lucapette <lucapette@gmail.com>
2018-10-11 14:15:55 +0200

    Add total count

--- zsh/kafka.zsh
+++ zsh/kafka.zsh
@@ -9,3 +9,7 @@ kafka-describe-all-consumer-groups() {
     kafka-consumer-groups --bootstrap-server localhost:9092 --describe --group $group
   done
 }
+
+kafka-topic-messages-count() {
+kafka-run-class kafka.tools.GetOffsetShell --broker-list localhost:9092 --topic $1 --time -1 | awk -F: '{total+=$3} END {print total}'
+}
