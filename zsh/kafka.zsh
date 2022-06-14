d56ab1c1de755d64fafa5875bb0d2d5f69551b0b
lucapette <ciao@lucapette.me>
2022-06-14 11:04:45 +0200

    Simple but usefuil kafka functions

--- zsh/kafka.zsh
+++ zsh/kafka.zsh
@@ -1,10 +1,11 @@
 export BOOTSTRAP_SERVER=localhost:9092
 export ZOOKEEPER=localhost:2181
+export KAFKA_HOME=$HOME/kafka
 
 kafka-start() {
 session_name="kafka-server-local"
 
-tmux new -d -s $session_name -n $session_name -c ~/kafka
+tmux new -d -s $session_name -n $session_name -c $KAFKA_HOME
 
 tmux send -t $session_name:1 "bin/zookeeper-server-start.sh config/zookeeper.properties" C-m
 
@@ -16,23 +17,36 @@ tmux attach
 }
 
 kafka-clean-console-groups() {
-for group in $(kafka-consumer-groups.sh --bootstrap-server ${BOOTSTRAP_SERVER:-localhost:9092} --list | grep console-); do
-  kafka-consumer-groups --bootstrap-server ${BOOTSTRAP_SERVER:-localhost:9092} --delete --group $group
+for group in $(${KAFKA_HOME}/bin/kafka-consumer-groups.sh --bootstrap-server ${BOOTSTRAP_SERVER} --list | grep console-); do
+  echo "deleting $group"
+  ${KAFKA_HOME}/bin/kafka-consumer-groups.sh --bootstrap-server ${BOOTSTRAP_SERVER} --delete --group $group
 done
 }
 
 kafka-describe-all-consumer-groups() {
-for group in $(kafka-consumer-groups.sh --bootstrap-server ${BOOTSTRAP_SERVER:-localhost:9092} --list); do
-  kafka-consumer-groups --bootstrap-server ${BOOTSTRAP_SERVER:-localhost:9092} --describe --group $group
+for group in $(${KAFKA_HOME}/bin/kafka-consumer-groups.sh --bootstrap-server ${BOOTSTRAP_SERVER} --list); do
+  ${KAFKA_HOME}/bin/kafka-consumer-groups.sh --bootstrap-server ${BOOTSTRAP_SERVER} --describe --group $group
 done
 }
 
+kafka-topics() {
+${KAFKA_HOME}/bin/kafka-topics.sh --bootstrap-server ${BOOTSTRAP_SERVER} $@
+}
+
+kafka-console-consumer() {
+${KAFKA_HOME}/bin/kafka-console-consumer.sh --bootstrap-server ${BOOTSTRAP_SERVER} $@
+}
+
+kafka-console-producer() {
+${KAFKA_HOME}/bin/kafka-console-producer.sh --bootstrap-server ${BOOTSTRAP_SERVER} $@
+}
+
 kafka-topic-messages-count() {
-kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list ${BOOTSTRAP_SERVER:-localhost:9092} --topic $1 --time -1 | awk -F: '{total+=$3} END {print total}'
+$KAFKA_HOME/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list ${BOOTSTRAP_SERVER} --topic $1 --time -1 | awk -F: '{total+=$3} END {print total}'
 }
 
 kafka-topics-messages-count-all() {
-for topic in $(kafka-topics.sh --bootstrap-server ${BOOTSTRAP_SERVER:-localhost:9092} --list | grep -v -e "^_"); do
+for topic in $(${KAFKA_HOME}/bin/kafka-topics.sh --bootstrap-server ${BOOTSTRAP_SERVER} --list | grep -v -e "^_"); do
   echo "$topic\t$(kafka-topic-messages-count $topic)"
 done
 }
