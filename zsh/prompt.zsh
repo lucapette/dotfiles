5d952e6af81748bda6abb81e5eedef900c70e3c0
lucapette <ciao@lucapette.me>
2022-12-17 00:57:05 +0100

    Ok let's go

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,14 +1,2 @@
-function powerline_precmd() {
-    PS1="$($HOME/bin/powerline-go -error $? -shell zsh -cwd-max-depth 1 -modules "kube,perms,cwd,git,jobs,exit,root" -theme default)"
-}
-
-function install_powerline_precmd() {
-  for s in "${precmd_functions[@]}"; do
-    if [ "$s" = "powerline_precmd" ]; then
-      return
-    fi
-  done
-  precmd_functions+=(powerline_precmd)
-}
-
-install_powerline_precmd
+export STARSHIP_CONFIG=~/.zsh/starship.toml
+eval "$(starship init zsh)"
