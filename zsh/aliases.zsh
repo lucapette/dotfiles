2719da1561b02bc19dd935cb1d5f144aef2df9f3
lucapette <ciao@lucapette.me>
2023-09-18 13:56:41 +0200

    little things

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,12 @@
-alias c='code .'
+alias c="code-insiders ."
 alias cat="bat --theme=\"$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo Nord || echo 'Solarized (light)')\""
-alias d='du -d1 -h'
-alias git='hub'
-alias k='kubectl'
-alias kcd='k config use-context'
-alias ls='ls --color=auto'
-alias mkdir='mkdir -p'
-alias rm='rm -i'
-alias tmux='tmux -2'
-alias v='g vi'
-alias vi='vim'
-alias vim='vim -O'
+alias code="code-insiders"
+alias d="du -d1 -h"
+alias git="hub"
+alias ls="ls --color=auto"
+alias mkdir="mkdir -p"
+alias rm="rm -i"
+alias tmux="tmux -2"
+alias v="g vi"
+alias vi="vim"
+alias vim="vim -O"
