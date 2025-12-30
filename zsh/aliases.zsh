222a60f1f60d1bf3de293dd942a64bd65076f71f
lucapette <ciao@lucapette.me>
2025-12-30 09:32:35 +0100

    no code

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,4 @@
-alias c="code-insiders ."
 alias cat="bat --theme=\"$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo Nord || echo 'Solarized (light)')\""
-alias code="code-insiders"
 alias d="du -d1 -h"
 alias git="hub"
 alias ls="ls --color=auto"
